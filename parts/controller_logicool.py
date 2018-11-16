cce9fdc79651cfa6986720514005c038dbf7fa4c
堀 扶 <89004@ad.exa-corp.co.jp>
2018-11-16 17:34:34 +0900

    コメント追加作業中のコードの一時保管

--- parts/controller_logicool.py
+++ parts/controller_logicool.py
@@ -1,5 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+controller_logicool.py
 
+Logicool F710 Wireless Joystick 用コントローラクラス
 
+本コードは https://github.com/mituhiromatuura/donkey よりダウンロード
+したもの(MITライセンス)に、日本語コメントを追記しています。
+"""
 import array
 import time
 import struct
@@ -9,19 +16,33 @@ import struct
 
 class Joystick():
     """
-    An interface to a physical joystick available at /dev/input
+    キャラクタデバイス /dev/input が有効となっている物理ジョイスティック
+    へのインターフェイスとなる。
     """
-    access_url = None #required to be consistent with web controller
+    access_url = None # Web コントローラとの一貫性のために定義
 
     def __init__(self, dev_fn='/dev/input/js0'):
+        """
+        コンストラクタ
+
+        引数：
+          dev_fn        キャラクタデバイスへのパス
+        """
+        # 軸状態辞書{軸名:状態}:init()にて書き込まれる
         self.axis_states = {}
+        # ボタン状態辞書{ボタン名,状態}:init()にて書き込まれる
         self.button_states = {}
+        # 軸マップ[軸名]:init()にて書き込まれる
         self.axis_map = []
+        # ボタンマップ[ボタン名]:init()にて書き込まれる
         self.button_map = []
+        # ジョイスティックキャラクタデバイス ファイルオブジェクト
         self.jsdev = None
+        # ジョイスティックキャラクタデバイス パス
         self.dev_fn = dev_fn
 
-        # These constants were borrowed from linux/input.h
+        # これらの定数は linux/input.h から借用
+        # キャラクタデバイス上のコードと軸名の辞書(F710固有)
         self.axis_names = {
             0x00 : 'x',
             0x01 : 'y',
@@ -50,7 +71,7 @@ class Joystick():
             0x20 : 'volume',
             0x28 : 'misc',
         }
-
+        # キャラクタデバイス上のコードとボタン名の辞書(F710固有)
         self.button_names = {
             0x120 : 'trigger',
             0x121 : 'thumb',
@@ -65,7 +86,7 @@ class Joystick():
             0x12a : 'base5',
             0x12b : 'base6',
 
-            #PS3 sixaxis specific
+            #PS3 Dualshock2 固有
             0x12c : "triangle",
             0x12d : "circle",
             0x12e : "cross",
@@ -92,7 +113,8 @@ class Joystick():
             0x222 : 'dpad_left',
             0x223 : 'dpad_right',
 
-            # XBox 360 controller uses these codes.
+            # XBox 360 controller ではこれらのコードを使用
+            # F710はXBox互換
             0x2c0 : 'dpad_left',
             0x2c1 : 'dpad_right',
             0x2c2 : 'dpad_up',
@@ -101,53 +123,79 @@ class Joystick():
 
 
     def init(self):
+        """
+        /dev/input/js0 への接続をセットアップし、ボタンのマッピングを行う
+        初期処理関数
+
+        引数
+          N/A
+        戻り値
+        　真偽値    常に真を返却
+        """
+        # 以下のパッケージはUNIX/Linuxにしかないため
+        # Windows 上で開くとエラー表示されることがある
         from fcntl import ioctl
-        """
-        call once to setup connection to dev/input/js0 and map buttons
-        """
-        # Open the joystick device.
+
+        # ジョイスティックキャラクタデバイスのオープン
         print('Opening %s...' % self.dev_fn)
         self.jsdev = open(self.dev_fn, 'rb')
 
-        # Get the device name.
+        # デバイス名の取得
         buf = array.array('B', [0] * 64)
+        # キャラクタデバイスからデバイス名を取得しbufへ書き込む
         ioctl(self.jsdev, 0x80006a13 + (0x10000 * len(buf)), buf) # JSIOCGNAME(len)
+        # バイト配列化後、UTF-8に変換し、デバイス名とする
         self.js_name = buf.tobytes().decode('utf-8')
         print('Device name: %s' % self.js_name)
 
-        # Get number of axes and buttons.
-        buf = array.array('B', [0])
+        # 軸やボタンの番号を取得
+        buf = array.array('B', [0]) # unsigned charを初期値[0]で配列化
+        # キャラクタデバイスから、軸数を取得しbufへ書き込む
         ioctl(self.jsdev, 0x80016a11, buf) # JSIOCGAXES
+        # 最初のバイト値を軸数とする
         self.num_axes = buf[0]
 
-        buf = array.array('B', [0])
+        buf = array.array('B', [0]) # unsigned charを初期値[0]で配列化
+        # キャラクタデバイスから、ボタン数を取得しbufへ書き込む
         ioctl(self.jsdev, 0x80016a12, buf) # JSIOCGBUTTONS
         self.num_buttons = buf[0]
 
-        # Get the axis map.
-        buf = array.array('B', [0] * 0x40)
+        # 軸マップの取得
+        buf = array.array('B', [0] * 0x40) # unsigned charを初期値 [0] * 0x40 で配列化
+        # キャラクタデバイスから軸名連結データを取得しbufへ書き込む
         ioctl(self.jsdev, 0x80406a32, buf) # JSIOCGAXMAP
 
+        # bufの先頭から軸数分のバイト配列を切り出し、1バイトづつ axisに格納するループ
         for axis in buf[:self.num_axes]:
+            # コンストラクタにて定義した辞書axis_namesをもとに軸名を取得する
+            # 存在しない場合は'unknown(0x%02x)' % axis形式の文字列で代替
             axis_name = self.axis_names.get(axis, 'unknown(0x%02x)' % axis)
+            # コンストラクタで空定義していた軸マップ(軸名の配列)に軸名を追加
             self.axis_map.append(axis_name)
+            # コンストラクタで空定義していた軸状態辞書(軸名の配列)に初期値の状態0.0をセット
             self.axis_states[axis_name] = 0.0
 
-        # Get the button map.
-        buf = array.array('H', [0] * 200)
+        # ボタンマップの取得
+        buf = array.array('H', [0] * 200) # unsigned charを初期値 [0] * 200 で配列化
         ioctl(self.jsdev, 0x80406a34, buf) # JSIOCGBTNMAP
 
+        # bufの先頭からボタン数分のバイト配列を切り出し、1バイトづつbtnに格納するループ
         for btn in buf[:self.num_buttons]:
+            # コンストラクタにて定義した辞書button_namesをもとにボタン名を取得する
+            # 存在しない場合は'unknown(0x%03x)' % axis形式の文字列で代替
             btn_name = self.button_names.get(btn, 'unknown(0x%03x)' % btn)
+            # コンストラクタで空定義していたボタンマップ(ボタン名の配列)にボタン名を追加
             self.button_map.append(btn_name)
+            # コンストラクタで空定義していたボタン状態辞書(ボタン名の配列)に初期値の状態0をセット
             self.button_states[btn_name] = 0
 
+        # すべて終了したら、真値を返却
         return True
 
 
     def show_map(self):
         """
-        list the buttons and axis found on this joystick
+        ジョイスティック上のすべてのボタンリストおよび軸リストを表示します。
         """
         print ('%d axes found: %s' % (self.num_axes, ', '.join(self.axis_map)))
         print ('%d buttons found: %s' % (self.num_buttons, ', '.join(self.button_map)))
@@ -155,45 +203,75 @@ class Joystick():
 
     def poll(self):
         """
-        query the state of the joystick, returns button which was pressed, if any,
-        and axis which was moved, if any. button_state will be None, 1, or 0 if no changes,
-        pressed, or released. axis_val will be a float from -1 to +1. button and axis will
-        be the string label determined by the axis map in init.
+        ジョイスティックの状態を照会し、押されたボタン、もしあれば移動した軸、
+        もしあれば移動したボタンを返します。 
+        button_stateの値は、変更なしの場合None、押下された場合 1 、
+        押していたボタンを話した場合 0 となります。
+        axis_val の値は -1 から +1 までの float となります。
+        ボタンと軸は、関数 init の軸マップにより決定する文字列ラベルとなります。
+
+        戻り値
+          button        ボタン名
+          button_state  ボタンの状態(None:変化なし, 1:押下, 0:押下状態からリリース)
+          axis          軸名
+          axsis_val     軸値(-1.0～1.0)
         """
+        # 戻り値用変数の定義
         button = None
         button_state = None
         axis = None
         axis_val = None
 
-        # Main event loop
+        # メインイベント ループ
+        # キャラクタデバイスから8バイト読み込む
         evbuf = self.jsdev.read(8)
 
+        # キャラクタデバイスから8バイト読み込めた場合
         if evbuf:
+            # evbufに格納されたバイト配列を書式文字列'IhBB'に従ってそれぞれ
+            # unsigned int tval, short value, unsigned char typev, unsigned char number
+            # にパックされたバイナリとして解釈された多胎を格納する
+            # なお先頭の tval は使用しない
             tval, value, typev, number = struct.unpack('IhBB', evbuf)
 
+            ＃初期化イベントの場合
             if typev & 0x80:
-                #ignore initialization event
+                # 無視
                 return button, button_state, axis, axis_val
 
+            # ボタンイベントの場合
             if typev & 0x01:
+                # ボタン名にする、ない場合None
                 button = self.button_map[number]
+                # 存在するボタン名の場合
                 if button:
+                    # ボタン状態をキャラクタデバイスから取得した値に変更
                     self.button_states[button] = value
                     button_state = value
 
+            # 軸イベントの場合
             if typev & 0x02:
+                # 軸名にする、ない場合None
                 axis = self.axis_map[number]
+                # 存在する軸名の場合
                 if axis:
+                    # 軸状態をキャラクタデバイスから取得した値を
+                    # -1.0から1.0の範囲内のfloat値にして変更
                     fvalue = value / 32767.0
                     self.axis_states[axis] = fvalue
                     axis_val = fvalue
 
+        # 戻り値を返却
         return button, button_state, axis, axis_val
 
 
 class JoystickController(object):
     """
-    Joystick client using access to local physical input
+    Logcool F710 ジョイスティックのPartクラス（Thredベース）
+    ローカルの物理デバイス(キャラクタデバイス)入力データへのアクセスを
+    利用するために用意された。
+    インスタンス変数群はジョイスティックで操作可能な値をすべて格納しており
+
     """
 
     def __init__(self, poll_delay=0.0,
@@ -206,6 +284,20 @@ class JoystickController(object):
                  pan_r_axis='rz',
                  dev_fn='/dev/input/js0',
                  auto_record_on_throttle=True):
+        """
+        コンストラクタ。
+
+        引数
+            poll_delay                  ポーリングの間隔(デフォルト:0.0)
+            max_throttle                最大スロットル値(デフォルト:1.0)
+            steering_axis               ステアリング軸名(デフォルト:'x')
+            steering_scale              ステアリング値(デフォルト:1.0)
+            throttle_scale              スロットル値(デフォルト:-1.0)
+            pan_l_axis                  
+            pan_r_axis
+            dev_fn
+            auto_record_on_throttle
+        """
 
         self.angle = 0.0
         self.throttle = 0.0
@@ -229,27 +321,42 @@ class JoystickController(object):
         self.dev_fn = dev_fn
         self.js = None
 
-        #We expect that the framework for parts will start a new
-        #thread for our update fn. We used to do that and it caused
-        #two threads to be polling for js events.
+        # このPartクラスはThread動作前提の実装のため、
+        # Parts フレームワークはファイル fn が更新されたら
+        # 新しいスレッドを開始すると想定して動作します。
+        # それを使用して、jsイベントのポーリングのために2つのスレッドを
+        # 発生させるようにしました。
 
     def on_throttle_changes(self):
         """
         turn on recording when non zero throttle in the user mode.
         """
+
+        #　スロットルオン時に記録フラグが真の場合
         if self.auto_record_on_throttle:
+            # 記録フラグを更新する。
+            # スロットルがオンであり、かつモードが'user'の場合のみ真値にする。
             self.recording = (self.throttle != 0.0 and self.mode == 'user')
 
     def init_js(self):
         """
-        attempt to init joystick
+        ジョイスティックの初期化を行う。
+
+        引数
+            なし
+        戻り値
+            真偽値  正常に初期化を完了したらTrue、例外発生時はFalseを返却
         """
         try:
+            # ジョイスティックインスタンスを生成
             self.js = Joystick(self.dev_fn)
+            # ジョイスティックインスタンスを初期化
             self.js.init()
+        # キャラクタデバイスが存在しない場合
         except FileNotFoundError:
             print(self.dev_fn, "not found.")
             self.js = None
+        # 正常に初期化を完了したらTrue、例外発生時はFalseを返却
         return self.js is not None
 
 
@@ -275,7 +382,9 @@ class JoystickController(object):
         * cross = PS3 cross => decrease max throttle
         """
 
-        #wait for joystick to be online
+        # ジョイスティックがオンラインになるまで待機するループ。
+        # ループ実行可否値および初期化完了を問い合わせ、
+        # 両方真値になるまで5秒間隔で繰り返す。
         while self.running and not self.init_js():
             time.sleep(5)
 
