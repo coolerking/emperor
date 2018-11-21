d77373bf65a9211d4e6d10473e5b2f1804267843
堀 扶 <89004@ad.exa-corp.co.jp>
2018-11-21 10:36:24 +0900

    キャラクタデバイスパス変更

--- check_elecom.py
+++ check_elecom.py
@@ -38,58 +38,57 @@ button_names = {
 
 def init():
     print('Opening %s...' % dev_fn)
-    jsdev = open(dev_fn, 'rb')
+    with open(dev_fn, 'rb') as jsdev:
 
-    # Get the device name.
-    buf = array.array('B', [0] * 64)
-    ioctl(jsdev, 0x80006a13 + (0x10000 * len(buf)), buf) # JSIOCGNAME(len)
-    js_name = buf.tobytes().decode('utf-8')
-    print('Device name: %s' % js_name)
+        # Get the device name.
+        buf = array.array('B', [0] * 64)
+        ioctl(jsdev, 0x80006a13 + (0x10000 * len(buf)), buf) # JSIOCGNAME(len)
+        js_name = buf.tobytes().decode('utf-8')
+        print('Device name: %s' % js_name)
 
-    # Get number of axes and buttons.
-    buf = array.array('B', [0])
-    ioctl(jsdev, 0x80016a11, buf) # JSIOCGAXES
-    num_axes = buf[0]
-    print('num_axes: ', num_axes)
+        # Get number of axes and buttons.
+        buf = array.array('B', [0])
+        ioctl(jsdev, 0x80016a11, buf) # JSIOCGAXES
+        num_axes = buf[0]
+        print('num_axes: ', num_axes)
 
-    buf = array.array('B', [0])
-    ioctl(jsdev, 0x80016a12, buf) # JSIOCGBUTTONS
-    num_buttons = buf[0]
-    print('num_buttons: ', num_buttons)
+        buf = array.array('B', [0])
+        ioctl(jsdev, 0x80016a12, buf) # JSIOCGBUTTONS
+        num_buttons = buf[0]
+        print('num_buttons: ', num_buttons)
 
-    # Get the axis map.
-    buf = array.array('B', [0] * 0x40)
-    ioctl(jsdev, 0x80406a32, buf) # JSIOCGAXMAP
+        # Get the axis map.
+        buf = array.array('B', [0] * 0x40)
+        ioctl(jsdev, 0x80406a32, buf) # JSIOCGAXMAP
 
-    print('***** axis_name list')
-    for axis in buf[:num_axes]:
-        axis_name = axis_names.get(axis, 'unknown(0x%02x)' % axis)
-        axis_map.append(axis_name)
-        axis_states[axis_name] = 0.0
-        print(' ', axis_name)
-        print('    = 0x%02x' % axis)
+        print('***** axis_name list')
+        for axis in buf[:num_axes]:
+            axis_name = axis_names.get(axis, 'unknown(0x%02x)' % axis)
+            axis_map.append(axis_name)
+            axis_states[axis_name] = 0.0
+            print(' ', axis_name)
+            print('    = 0x%02x' % axis)
 
-    # Get the button map.
-    buf = array.array('H', [0] * 200)
-    ioctl(jsdev, 0x80406a34, buf) # JSIOCGBTNMAP
+        # Get the button map.
+        buf = array.array('H', [0] * 200)
+        ioctl(jsdev, 0x80406a34, buf) # JSIOCGBTNMAP
 
-    print('***** button_name list')
-    for btn in buf[:num_buttons]:
-        btn_name = button_names.get(btn, 'unknown(0x%03x)' % btn)
-        button_map.append(btn_name)
-        button_states[btn_name] = 0
-        print(' ', btn_name)
-        print('    = 0x%03x' % btn)
+        print('***** button_name list')
+        for btn in buf[:num_buttons]:
+            btn_name = button_names.get(btn, 'unknown(0x%03x)' % btn)
+            button_map.append(btn_name)
+            button_states[btn_name] = 0
+            print(' ', btn_name)
+            print('    = 0x%03x' % btn)
 
-    EVENT_FORMAT = "llHHI"; # long, long, unsigned short, unsigned short, unsigned int
-    EVENT_SIZE = struct.calcsize(EVENT_FORMAT)
+        EVENT_FORMAT = "llHHI"; # long, long, unsigned short, unsigned short, unsigned int
+        EVENT_SIZE = struct.calcsize(EVENT_FORMAT)
 
-    with open('/dev/input/event0', "rb") as file:
-        event = file.read(EVENT_SIZE)
+        event = jsdev.read(EVENT_SIZE)
         while event:
             #(tv_sec, tv_usec, type, code, value) = struct.unpack(EVENT_FORMAT, event)
             print(struct.unpack(EVENT_FORMAT, event))
-            event = file.read(EVENT_SIZE)
+            event = jsdev.read(EVENT_SIZE)
 
 if __name__ == '__main__':
     init()
