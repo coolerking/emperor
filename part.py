002de11a4a64d64fdf59620d48bdc1c70ed7660e
堀 扶 <89004@ad.exa-corp.co.jp>
2018-12-27 09:26:58 +0900

    binary_to_img の試行

--- part.py
+++ part.py
@@ -7,12 +7,20 @@ class ImageChecker:
     def run(self, image):
         print('***** ImageChecker *****')
         #print(image)
-        print(type(image))
+        #print(type(image))
         if type(image) is np.ndarray:
-            print(image.shape)
+            #print(image.shape)
             bin = dk.util.img.arr_to_binary(image)
-            print(bin)
-            print(type(bin))
-            print(len(bin))
+            #print(bin)
+            #print(type(bin))
+            #print(len(bin))
+            print(image.dtype)
+            # PiCamera -> np.ndarray型式 (120,160,3)
+            image2 = dk.util.img.binary_to_img(bin)
+            print('*** image2')
+            print(image2)
+            print(type(image2))
+            print(image == image2)
+
         print('************************')
 
