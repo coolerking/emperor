73b2531472c410af2b753b4ed672f2d721f13f39
堀 扶 <89004@ad.exa-corp.co.jp>
2018-12-27 09:12:37 +0900

    デバッグコード修正

--- part.py
+++ part.py
@@ -5,8 +5,10 @@ class ImageChecker:
     def __init__(self):
         pass
     def run(self, image):
-        print(image)
+        print('***** ImageChecker *****')
+        #print(image)
         print(type(image))
-        if type(image) is np.array:
+        if type(image) is np.ndarray:
             print(image.shape)
+        print('************************')
 
